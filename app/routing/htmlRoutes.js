ab03c2bafa8a92f63eb880594536ec370292b363
elioye27 <elioye27@gmail.com>
2017-11-19 02:28:01 -0800

    add codes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,12 +14,17 @@ module.exports = function(app) {
   // Below code handles when users "visit" a page.
   // ---------------------------------------------------------------------------
 
+
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+
 
 // Sets up the Express App
 // =============================================================
@@ -13,7 +13,16 @@ var PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
 
 // Starts the server to begin listening
 // =============================================================
