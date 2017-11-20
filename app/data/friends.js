157e86faf443f27facb59be8d92db1ab5945daa4
elioye27 <elioye27@gmail.com>
2017-11-19 19:43:33 -0800

    modfified codes

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,9 +5,9 @@
 
 var friends = [
 	{
-	  "name":"Ahmed",
-	  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-	  "scores":[
+	  name:"Ahmed",
+	  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	  scores:[
 	      5,
 	      1,
 	      4,
@@ -18,8 +18,8 @@ var friends = [
 	      5,
 	      4,
 	      1
-	    ]
-	}
+	    	]
+	},
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,6 @@
+
+
+
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
@@ -32,12 +35,44 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
-    	// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    	// It will do this by sending out the value "true" have a table
-    	// req.body is available since we're using the body-parser middleware
-      friends.push(req.body);
-      res.json(friends);
-  	});
+  	// Add new friend entry
+	app.post('/api/friends', function(req, res) {
+		// Capture the user input object
+		var newInfo = req.body;
+		console.log('New Information = ' + JSON.stringify(newInfo));
+
+    	var scoresArray = [];
+    	var friendCount = 0;
+    	var bestMatch = 0;
+
+
+    	//runs through all current friends in list
+		for(var i=0; i<friends.length; i++) {
+			var scoresDiff = 0;
+		}
+			
+		 //after all friends are compared, find best match
+		for (var j = 0; j < newInfo.length; j++) {
+				scoresDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newInfo[j])));
+
+				scoresArray.push(scoresDiff);
+		}
+			
+
+		// If lowest difference, record the friend match
+		for(var i=0; i<scoresArray.length; i++){
+  			if(scoresArray[i] <= scoresArray[bestMatch]){
+    			
+    			bestMatch = i;
+  			}
+		}
+
+	 	//return bestMatch data
+	    var bestMatch = friends[bestMatch];
+	    res.json(bestMatch);
+
+	    //pushes new submission into the friends array
+	    friends.push(req.body);
+	});
 };
 
